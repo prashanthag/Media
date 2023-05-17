bb3e3410cb4c0836f527fb92c2fd0bc52867d667
Prashantha Gowda <prashantha.g@gmail.com>
2023-05-17 09:01:02 -0700

    * ffmpeg and preinstall stuff updated

--- opal/script/ffmpeg.sh
+++ opal/script/ffmpeg.sh
@@ -1,3 +1,4 @@
+. ../env_install.sh
 cd ../
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 cd nv-codec-headers
@@ -14,7 +15,7 @@ cd ../
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
 mkdir ffmpeg/gpl
 cd ffmpeg/gpl
-../configure  --extra-ldflags="-L/usr/local/lib -L/usr/local/cuda/lib64" --extra-cflags="-I/usr/local/include -I/usr/local/cuda/include"  --enable-shared --enable-gpl --enable-libx264  --enable-cuda --enable-cuvid --enable-nvenc --enable-nonfree --enable-libnpp  --enable-libfreetype --enable-libfontconfig --enable-libfribidi
+../configure  --extra-ldflags="-L/${INSTALL_DIR}/lib -L/${INSTALL_DIR}/cuda/lib64" --extra-cflags="-I/${INSTALL_DIR}/include -I/${INSTALL_DIR}/cuda/include"  --enable-shared --enable-gpl --enable-libx264  --enable-cuda --enable-cuvid --enable-nvenc --enable-nonfree --enable-libnpp  --enable-libfreetype --enable-libfontconfig --enable-libfribidi
 make -j $(nproc -all)
 sudo make install 
 cd ../../
--- opal/script/preinstall_conf.sh
+++ opal/script/preinstall_conf.sh
@@ -1,5 +1,4 @@
-git clone git://git.code.sf.net/p/opalvoip/ptlib ../ptlib
-git clone git://git.code.sf.net/p/opalvoip/opal  ../opal
-sudo apt install -y g++ git make autoconf libpcap-dev libexpat-dev libssl-dev libsasl2-dev libldap-dev  unixodbc-dev liblua5.3-dev libv8-dev libncurses-dev libsdl2-dev libavformat-dev  libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt install -y libwxgtk3.0-gtk3-dev libsrtp2-dev libgsm1-dev libspeex-dev libopus-dev libavcodec-dev libx264-dev libvpx-dev libtheora-dev libspandsp-dev capiutils dahdi
+#DEBIAN_FRONTEND=noninteractive apt install -y libavformat-dev libavcodec-dev libx264-dev 
+DEBIAN_FRONTEND=noninteractive apt install -y libv8-dev libspeex-dev libopus-dev   libvpx-dev libtheora-dev libspandsp-dev 
+DEBIAN_FRONTEND=noninteractive apt install -y vim g++ git make autoconf libpcap-dev libexpat-dev libssl-dev libsasl2-dev libldap-dev  unixodbc-dev liblua5.3-dev  libncurses-dev libsdl2-dev  libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libwxgtk3.0-gtk3-dev libsrtp2-dev libgsm1-dev  capiutils dahdi 
 
